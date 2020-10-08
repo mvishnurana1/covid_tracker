7125cc2031e94af7c0cc68e9b8ac07eb1ac2ca63
Vishnu Rana <mvishnurana@gmail.com>
2020-10-08 23:20:08 +0800

    Changed component to become a view-only component by removing class to become functional

--- src/component/View/CountryData.jsx
+++ src/component/View/CountryData.jsx
@@ -1,10 +1,33 @@
-import React, { Component } from 'react'; 
+import CovidLogo from '../../assets/virus.svg'; 
+import moment from 'moment'; 
+import React from 'react'; 
 import Spinner from 'react-bootstrap/Spinner'; 
 
-class CountryData extends Component {
-  render() {
-    return <Spinner animation="grow" />
-  } 
+import './CountryData.css'; 
+
+const CountryData = ({ countryInfo }) => {
+  if (!countryInfo) {
+    console.log('no data, returning ');
+    return <div>
+      <Spinner />
+    </div> 
+  }
+
+  return <div style={{ backgroundColor: 'pink'}}>
+    <img alt="" className='imageStyle' src={CovidLogo} />
+    <div>
+      {countryInfo.CountryCode}
+    </div>
+    <div>
+      {countryInfo.Country}
+    </div>
+    <div>
+      {countryInfo.Cases}
+    </div>
+    <div>
+      {moment(countryInfo.Date).format('DD/MM/YYYY')}
+    </div>
+  </div>
 }
 
 export default CountryData; 
