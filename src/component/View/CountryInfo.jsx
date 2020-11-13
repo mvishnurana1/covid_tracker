a09bf496d01a2bee89b7423b719fa6cfea6d3ba7
Vishnu Rana <mvishnurana@gmail.com>
2020-11-13 21:01:06 +0800

    Added card view to the country info page, added country flag logo on successful search and leverages moment.js to render the date returned from the API

--- src/component/View/CountryInfo.jsx
+++ src/component/View/CountryInfo.jsx
@@ -1,32 +1,33 @@
 import moment from 'moment'; 
 import React from 'react'; 
+import { Card, Image } from 'react-bootstrap'; 
 
 import './CountryInfo.css'; 
 
 import CovidLogo from '../../assets/virus.svg'; 
 
-const CountryData = ({ countryInfo }) => {
-  if (!countryInfo) {
-    return <div>
-      No Data available...
-    </div>  
+const CountryInfo = ({ data }) => {
+  if (!data) {
+    console.log('No data dound...'); 
+    return; 
   }
 
-  return <div style={{ backgroundColor: 'pink'}}>
+  return <Card style={{ backgroundColor: 'pink', width: '500px'}}>
     <img alt="" className='imageStyle' src={CovidLogo} />
     <div>
-      {countryInfo.CountryCode}
+      {data[data.length - 1].Country}
+      <Image 
+        src={`https://www.countryflags.io/${data[data.length - 1].CountryCode}/shiny/64.png`}
+        fluid
+      />
     </div>
     <div>
-      {countryInfo.Country}
+      {(data[data.length - 1].Cases).toLocaleString()}
     </div>
     <div>
-      {countryInfo.Cases}
+      {moment(data[data.length - 1].Date).format('LLLL')}
     </div>
-    <div>
-      {moment(countryInfo.Date).format('DD/MM/YYYY')}
-    </div>
-  </div>
+  </Card>
 }
 
-export default CountryData; 
+export default CountryInfo; 
