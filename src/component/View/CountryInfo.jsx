a3f8fe4c0becf9a47d772c383aa362d55d085594
Vishnu Rana <mvishnurana@gmail.com>
2020-11-14 14:00:18 +0800

    Fixed styling of Card component and layout of the content renent rendered in the Card

--- src/component/View/CountryInfo.jsx
+++ src/component/View/CountryInfo.jsx
@@ -1,10 +1,8 @@
 import moment from 'moment'; 
 import React from 'react'; 
-import { Card, Image } from 'react-bootstrap'; 
+import { Card, Image, ListGroup } from 'react-bootstrap'; 
 
 import './CountryInfo.css'; 
-
-import CovidLogo from '../../assets/virus.svg'; 
 
 const CountryInfo = ({ data }) => {
   if (!data) {
@@ -14,21 +12,43 @@ const CountryInfo = ({ data }) => {
 
   const latestUpdate = data[data.length - 1]; 
 
-  return <Card style={{ backgroundColor: 'pink', width: '500px'}}>
-    <img alt="" className='imageStyle' src={CovidLogo} />
-    <div>
-      {latestUpdate.Country}
-      <Image 
-        src={`https://www.countryflags.io/${latestUpdate.CountryCode}/shiny/64.png`}
-        fluid
-      />
-    </div>
-    <div>
-      {(latestUpdate.Cases).toLocaleString()}
-    </div>
-    <div>
-      {moment(latestUpdate.Date).format('LLLL')}
-    </div>
+  return <Card style={{ backgroundColor: 'pink', width: '20rem'}}>
+    <ListGroup variant="flush">
+    <ListGroup.Item>
+      <div style={{ alignItems: 'center', display: 'flex', justifyContent: 'space-evenly' }}>
+        <div style={{ fontWeight: '600', textAlign: 'right', width: '8rem'}}>
+          Country: 
+        </div>
+        <div style={{ alignItems: 'center', display: 'flex', marginLeft: '1rem', width: '9rem' }}>
+          {latestUpdate.Country}
+          <Image 
+            src={`https://www.countryflags.io/${latestUpdate.CountryCode}/shiny/64.png`}
+            fluid
+          />
+        </div>
+      </div>
+    </ListGroup.Item>
+    <ListGroup.Item>
+      <div style={{ alignItems: 'center', display: 'flex', height: '64px', justifyContent: 'space-evenly' }}>
+        <div style={{ fontWeight: '600', textAlign: 'right', width: '8rem'}}>
+        Cases to Date: 
+        </div>
+        <div style={{ alignItems: 'center', display: 'flex', marginLeft: '1rem', width: '9rem' }}>
+          {(latestUpdate.Cases).toLocaleString()}
+        </div>
+      </div>
+    </ListGroup.Item>
+    <ListGroup.Item>
+    <div style={{ alignItems: 'center', display: 'flex', height: '64px',  justifyContent: 'space-evenly' }}>
+        <div style={{ fontWeight: '600', textAlign: 'right', width: '8rem'}}>
+          Updated On: 
+        </div>
+        <div style={{ alignItems: 'center', display: 'flex', marginLeft: '1rem', width: '9rem' }}>
+          {moment(latestUpdate.Date).format('LL')}
+        </div>
+      </div>
+    </ListGroup.Item>
+  </ListGroup>
   </Card>
 }
 
