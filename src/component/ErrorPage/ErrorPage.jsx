d1243044b88122a0e91b17b4c45b6bf80531d209
Vishnu Rana <mvishnurana@gmail.com>
2020-11-14 18:16:36 +0800

    Removed inline styling and updated stylesheet

--- src/component/ErrorPage/ErrorPage.jsx
+++ src/component/ErrorPage/ErrorPage.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 
+import './ErrorPage.css'; 
+
 const ErrorPage = ({ country, error }) => {
-    return (<div style={{ width: '25rem'}}>
+    return (<div className="pageLayout">
         <h2>Encountered Error: {error.status}</h2> 
         <h2>While looking for Country: {country}</h2>
     </div>); 
