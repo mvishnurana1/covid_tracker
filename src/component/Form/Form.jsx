9bcada273d7f38b9e77ced975fbad22180be5314
Vishnu Rana <mvishnurana@gmail.com>
2020-11-15 13:45:46 +0800

    Moved away styles to the components stylesheet

--- src/component/Form/Form.jsx
+++ src/component/Form/Form.jsx
@@ -61,26 +61,25 @@ class Forms extends Component {
   render() {
     return (
     <div className="page">
-    <Form className="formContainer" onSubmit={(e) => this.handleSubmit(e)}> 
-      <Form.Group className='formAlignment'> 
-        <Form.Control 
-          size="lg" 
-          style={{ marginRight: '2rem' }}
-          placeholder="Enter Country"
-          onChange={(e) => this.handleChange(e)}
-          country={this.state.country}
-          value={this.state.country}
-        />
-        <Button 
-          variant="primary"  
-          value="country"
-          type="submit">
-          Submit
-        </Button>
-      </Form.Group>
-    </Form>
-    {this.renderCountryInformation()}
-    {this.renderCovidApiErrorPage()}
+      <Form className="formContainer" onSubmit={(e) => this.handleSubmit(e)}> 
+        <Form.Group className='formAlignment'> 
+          <Form.Control 
+            size="lg" 
+            placeholder="Enter Country"
+            onChange={(e) => this.handleChange(e)}
+            country={this.state.country}
+            value={this.state.country}
+          />
+          <Button 
+            className="submit"
+            value="country"
+            type="submit">
+            Submit
+          </Button>
+        </Form.Group>
+      </Form>
+      {this.renderCountryInformation()}
+      {this.renderCovidApiErrorPage()}
     </div>
   )}
 }
